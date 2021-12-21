1cc924f2cb0baa6b1508ab3660b8160503b06b91
Vitor Calejuri <vitor.cajuleri@gmail.com>
2021-12-21 14:45:13 -0300

    Improved Header layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,24 @@
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 
+import SvgPlaceholder from '../SvgPlaceholder';
+
 import { observer } from 'mobx-react-lite';
 import { useFundsStore } from '$features/funds/store';
+import { cn } from '$lib/utils';
 
 const Header = () => {
   const funds = useFundsStore();
   return (
     <header className={styles.header}>
-      Header
-      <span id="balance">R$ {funds?.balance}</span>
+      <div className={cn('pageWrapper', styles.pageWrapper)}>
+        <Link to="/" className={styles.logo}>
+          <img src={SvgPlaceholder({ width: 100, height: 104 })} />
+        </Link>
+        <div className={styles.right}>
+          <span id="balance">R$ {funds?.balance}</span>
+        </div>
+      </div>
     </header>
   );
 };
